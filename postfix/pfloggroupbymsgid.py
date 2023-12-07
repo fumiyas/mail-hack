b760542f76ecbf59ea3eed4907faeb6373ad7f3a
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2023-12-07 22:46:57 +0900

    feat: pfloggroupbymsgid: Support service prefix
    E.g., `submission/smtpd`

--- postfix/pfloggroupbymsgid.py
+++ postfix/pfloggroupbymsgid.py
@@ -14,8 +14,11 @@ import re
 from collections import OrderedDict
 
 log_re = re.compile(
-    r'^.* postfix/(?P<service>[a-z]+)\[(?P<pid>[0-9]+)\]: '
-    r'((?P<qid>[0-9A-F]{6,}|[0-9B-DF-HJ-NP-TV-Zb-df-hj-np-tv-y]{10,}z[0-9B-DF-HJ-NP-TV-Zb-df-hj-np-tv-y]+): )(?P<content>.*)$'
+    r'^'
+    r'(?P<timestamp>[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-1][0-9]:[0-5][0-9]:[0-5][0-9](\.[0-9]+)?[-+][0-1][0-9]:[0-5][0-9]|[^A-Z][a-z][a-z] [ 1-3][0-9] [0-1][0-9]:[0-5][0-9]:[0-5][0-9]) '
+    r'(?P<hostname>[-._A-Za-z0-9]+) '
+    r'postfix/(?:(?P<service_prefix>[-._0-9a-z]+/)?(?P<service>[-._0-9a-z]+))\[(?P<pid>[0-9]+)\]: '
+    r'(?:(?P<qid>[0-9A-F]{6,}|[0-9B-DF-HJ-NP-TV-Zb-df-hj-np-tv-y]{10,}z[0-9B-DF-HJ-NP-TV-Zb-df-hj-np-tv-y]+): )(?P<content>.*)$'
 )
 log_cleanup_msgid_re = re.compile(r'^message-id=<(?P<msgid>.*?)>$')
 
@@ -35,8 +38,10 @@ for line in sys.stdin:
     qid = m.group('qid')
     log = {
         'line': line,
+        'timestamp': m.group('timestamp'),
         'pid': m.group('pid'),
         'qid': qid,
+        'service_prefix': m.group('service_prefix'),
         'service': m.group('service'),
         'content': m.group('content'),
     }
@@ -59,8 +64,8 @@ for line in sys.stdin:
 
 ## FIXME: Print pending queue logs
 for msgid, logs_list in logs_list_by_msgid.items():
-    print('Message-ID: <%s>' % (msgid))
+    print(f"Message-ID: <{msgid}>")
     for logs in logs_list:
-        print('  Queue ID:', logs[0]['qid'])
+        print(f"  Queue ID: {logs[0]['qid']}")
         for log in logs:
-            print('    ', log['line'])
+            print(f"    {log['timestamp']} {log['service']} {log['content']}")
