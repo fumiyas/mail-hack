9dc0aa539937e9a17d25a31532c5ad09841bc911
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-04-24 18:46:40 +0900

    feat: pfholdlogsum.sh: New

---
+++ postfix/pfholdlogsum.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+##
+## Postfix: Summarize header_checks(5) hold logs
+##
+## SPDX-FileCopyrightText: 2018-2024 SATOH Fumiyasu @ OSSTech Corp., Japan
+## SPDX-License-Identifier: GPL-3.0-or-later
+##
+
+if [ "${1-}" = "-h" ]; then
+  echo "Usage: ${0##*/} [MAIL.LOG]"
+  exit 1
+fi
+
+if [ $# -eq 0 ]; then
+  set -- /var/log/mail.log
+elif [ "${1-}" = "-" ]; then
+  shift $#
+fi
+
+grep ' hold: header ' "$@" \
+|sed -E \
+  -e 's/.* hold: header //' \
+  -e 's/ from [^ ]+;( [a-z]+=[^ ]+)*:/\t/' \
+  -e '/^From:/s/ <[^\t]+\t/ | /' \
+  -e '/^List-Unsubscribe:/s/<mailto:[^@>]*@[^.>]*\?[^>]*>/<mailto:...>/' \
+  -e 's/(=\?[-_A-Za-z0-9]+\?[BbQq]\?[^?]+\?=)\?+/\1/g' \
+|(
+  if type nkf >/dev/null 2>&1; then
+    exec nkf -w
+  else
+    exec cat
+  fi
+) \
+|sed -E \
+  -e 's/[ \t]+/ /g' \
+|sort \
+|uniq -c \
+|sort -nr \
+;
